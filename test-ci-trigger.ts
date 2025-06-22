e5066a6531826f37be4efb74cbe1754819e0c71b
Umutcan ÖNER <uo@uozopio.com>
2025-06-23 02:33:41 +0300

    test: add CI trigger test file

---
+++ test-ci-trigger.ts
@@ -0,0 +1,7 @@
+// Test file for CI workflow trigger testing
+export function testCITrigger(): string {
+  return 'Testing CI workflow triggers';
+}
+
+// This file is used to test various CI workflow scenarios
+console.log(testCITrigger());
